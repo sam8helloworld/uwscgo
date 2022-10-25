83ff56502a552f10bc219ac992658607003ae29e
sam8helloworld <samcreate0830@gmail.com>
2022-10-25 21:42:12 +0900

    test: 識別子の構文解析

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -79,3 +79,32 @@ func checkParserErrors(t *testing.T, p *parser.Parser) {
 	}
 	t.FailNow()
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := `val`
+
+	l := lexer.NewLexer(input)
+	p := parser.NewParser(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp is not ast.ExpressionStatement. got=%T", stmt.Expression)
+	}
+	if ident.Value != "val" {
+		t.Errorf("ident.Value not %s. got=%s", "val", ident.Value)
+	}
+	if ident.TokenLiteral() != "val" {
+		t.Errorf("ident.TokenLiteral() not %s. got=%s", "val", ident.TokenLiteral())
+	}
+}
