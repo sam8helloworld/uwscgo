f9a7c064818f2a38646b4f52065d9b167408348d
sam8helloworld <samcreate0830@gmail.com>
2022-10-26 01:06:40 +0900

    test: 整数リテラルを評価する

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,53 @@
+package evaluator_test
+
+import (
+	"testing"
+
+	"github.com/sam8helloworld/uwscgo/evaluator"
+	"github.com/sam8helloworld/uwscgo/lexer"
+	"github.com/sam8helloworld/uwscgo/object"
+	"github.com/sam8helloworld/uwscgo/parser"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected int64
+	}{
+		{
+			"整数を評価できる",
+			"5",
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			testIntegerObject(t, evaluated, tt.expected)
+		})
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.NewLexer(input)
+	p := parser.NewParser(l)
+	program := p.ParseProgram()
+
+	return evaluator.Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer.got=%T (%+T)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
