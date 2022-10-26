904c5af1183a87a01de7344c029d8eefa8b18b1c
sam8helloworld <samcreate0830@gmail.com>
2022-10-26 22:03:19 +0900

    feat: DIMの変数を環境に束縛

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,24 +5,37 @@ import (
 	"github.com/sam8helloworld/uwscgo/object"
 )
 
-func Eval(node ast.Node) object.Object {
+func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalStatements(node.Statements, env)
 	case *ast.ExpressionStatement:
-		return Eval(node.Expression)
+		return Eval(node.Expression, env)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.DimStatement:
+		val := Eval(node.Value, env)
+		env.Set(node.Name.Value, val)
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
 	}
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalStatements(stmts []ast.Statement, env *object.Environment) object.Object {
 	var result object.Object
 
 	for _, statement := range stmts {
-		result = Eval(statement)
+		result = Eval(statement, env)
 	}
 
 	return result
+}
+
+func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return nil
+	}
+	return val
 }
