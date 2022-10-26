b1245f84df87c911eb79b9dcac6a066755f9d4ab
sam8helloworld <samcreate0830@gmail.com>
2022-10-26 22:03:12 +0900

    test: DIMの変数を環境に束縛

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -34,8 +34,9 @@ func testEval(input string) object.Object {
 	l := lexer.NewLexer(input)
 	p := parser.NewParser(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return evaluator.Eval(program)
+	return evaluator.Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
@@ -50,4 +51,29 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 		return false
 	}
 	return true
+}
+
+func TestDimStatements(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected int64
+	}{
+		{
+			"整数の変数定義(1つ)",
+			"DIM val = 5;val;",
+			5,
+		},
+		{
+			"整数の変数を整数の変数に代入",
+			"DIM valA = 5;DIM valB = valA;valB",
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testIntegerObject(t, testEval(tt.input), tt.expected)
+		})
+	}
 }
