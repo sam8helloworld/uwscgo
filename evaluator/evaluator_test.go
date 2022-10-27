af5e80e376182f4f50395a199baceb7d2a0d837e
sam8helloworld <samcreate0830@gmail.com>
2022-10-27 19:22:32 +0900

    test: 真偽値を評価する

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -110,3 +110,42 @@ valB`,
 		})
 	}
 }
+
+func TestBooleanExpression(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			"TRUE",
+			"TRUE",
+			true,
+		},
+		{
+			"FALSE",
+			"FALSE",
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			testBoolenObject(t, evaluated, tt.expected)
+		})
+	}
+}
+
+func testBoolenObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Fatalf("object is not Boolean. got=%T (%+T)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+		return false
+	}
+	return true
+}
