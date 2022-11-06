a026f4dc595258c39c0b179327094e79d46c31b8
sam8helloworld <samcreate0830@gmail.com>
2022-11-06 23:17:40 +0900

    feat: LENGTH関数を組み込む

---
+++ evaluator/builtins.go
@@ -0,0 +1,23 @@
+package evaluator
+
+import (
+	"unicode/utf8"
+
+	"github.com/sam8helloworld/uwscgo/object"
+)
+
+var builtins = map[string]*object.Builtin{
+	"LENGTH": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(utf8.RuneCountInString(arg.Value))}
+			default:
+				return newError("argument to `LENGTH` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+}
