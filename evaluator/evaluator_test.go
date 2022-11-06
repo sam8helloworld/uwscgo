a314da17bd202f044f2704997801bd49ce99307e
sam8helloworld <samcreate0830@gmail.com>
2022-11-06 22:38:24 +0900

    test: 文字列の結合を評価する

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -432,6 +432,11 @@ func TestErrorHandling(t *testing.T) {
 			"TRUE + FALSE",
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"文字列同士の予期せぬ中置演算子",
+			`"Hello" - "World!"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -458,6 +463,20 @@ func TestStringLiteral(t *testing.T) {
 	}
 
 	if str.Value != "Hello World" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
