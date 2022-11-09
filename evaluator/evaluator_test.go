72c0d7f74191b7cc4d753b55ec637f34c9a6f56f
sam8helloworld <samcreate0830@gmail.com>
2022-11-10 00:41:16 +0900

    test: 配列定義を評価する

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -518,3 +518,23 @@ func TestBuiltinFunctions(t *testing.T) {
 		})
 	}
 }
+
+func TestArrayLiterals(t *testing.T) {
+	input := `DIM array[2] = 1, 2 * 2, 3 + 3
+array
+	`
+
+	evaluated := testEval(input)
+	array, ok := evaluated.(*object.Array)
+	if !ok {
+		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(array.Elements) != 3 {
+		t.Fatalf("array has wrong num of elements. got=%d", len(array.Elements))
+	}
+
+	testIntegerObject(t, array.Elements[0], 1)
+	testIntegerObject(t, array.Elements[1], 4)
+	testIntegerObject(t, array.Elements[2], 6)
+}
