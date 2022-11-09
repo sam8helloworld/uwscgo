3eb90aed04b845f50bb59a6eb4af8094d12bc34d
sam8helloworld <samcreate0830@gmail.com>
2022-11-10 01:38:10 +0900

    feat: LENGTH関数を配列に対応

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -15,6 +15,8 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(utf8.RuneCountInString(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `LENGTH` not supported, got %s", args[0].Type())
 			}
