413d088701a972f36d440cad0df8c5e9d8d06cff
sam8helloworld <samcreate0830@gmail.com>
2022-11-16 05:59:11 +0900

    feat: 連想配列のオブジェクトを作成

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/sam8helloworld/uwscgo/ast"
@@ -22,6 +23,7 @@ const (
 	BUILTIN_FUNCTION_OBJ              = "BUILTIN_FUNCTION_OBJ"
 	BUILTIN_CONSTANT_OBJ              = "BUILTIN_CONSTANT_OBJ"
 	ARRAY_OBJ                         = "ARRAY"
+	HASHTBL_OBJ                       = "HASHTBL_OBJ"
 	BUILTIN_FUNC_RETURN_RESULT_OBJ    = "BUILTIN_FUNC_RETURN_RESULT"
 	BUILTIN_FUNC_RETURN_REFERENCE_OBJ = "BUILTIN_FUNC_RETURN_REFERENCE"
 )
@@ -233,6 +235,66 @@ func (b *BuiltinFuncReturnReference) Inspect() string {
 	out.WriteString("result")
 	out.WriteString("=")
 	out.WriteString(b.Result.Inspect())
+	out.WriteString("}")
+	return out.String()
+}
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+type HashPair struct {
+	Key   Object
+	Value Object
+}
+
+type Hashable interface {
+	HashKey() HashKey
+}
+
+type HashTable struct {
+	Pairs    map[HashKey]HashPair
+	Sort     bool
+	Casecare bool
+}
+
+func (ht *HashTable) Type() ObjectType {
+	return HASHTBL_OBJ
+}
+
+func (ht *HashTable) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range ht.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
 	out.WriteString("}")
 	return out.String()
 }
