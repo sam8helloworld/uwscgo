b2807dc0ea7d9041142b3c244f4ec935750e2c4c
sam8helloworld <samcreate0830@gmail.com>
2022-11-17 05:21:12 +0900

    test: 順列番号で連想配列のペアを取得する

--- object/object_test.go
+++ object/object_test.go
@@ -24,3 +24,53 @@ func TestStringHashKey(t *testing.T) {
 		t.Errorf("strings with same content have same hash keys")
 	}
 }
+
+func TestHashTableGeyPairByIndex(t *testing.T) {
+	ht := &object.HashTable{
+		Pairs: map[object.HashKey]object.HashPair{
+			{
+				Type:  object.STRING_OBJ,
+				Value: uint64(12638189399578898418),
+			}: {
+				Key: &object.String{
+					Value: "c",
+				},
+				Value: &object.Integer{
+					Value: 3,
+				},
+			},
+			{
+				Type:  object.STRING_OBJ,
+				Value: uint64(12638187200555641996),
+			}: {
+				Key: &object.String{
+					Value: "a",
+				},
+				Value: &object.Integer{
+					Value: 1,
+				},
+			},
+			{
+				Type:  object.STRING_OBJ,
+				Value: uint64(12638190499090526629),
+			}: {
+				Key: &object.String{
+					Value: "b",
+				},
+				Value: &object.Integer{
+					Value: 2,
+				},
+			},
+		},
+		IsSort: true,
+	}
+
+	pair := ht.GetPairByIndex(1)
+	key, ok := pair.Key.(*object.String)
+	if !ok {
+		t.Fatalf("pair.Key is not *object.String. got=%T", pair.Key)
+	}
+	if key.Value != "b" {
+		t.Errorf("key.Value is not 'b'. got=%s", key.Value)
+	}
+}
